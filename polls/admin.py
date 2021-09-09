2362b3d5a70520ef178a81552aa2acb9a100f685
Kittison <kittison.j@ku.th>
2021-09-09 16:29:29 +0700

    Add link iteration 2, end_date and two method in Question model

--- polls/admin.py
+++ polls/admin.py
@@ -11,12 +11,12 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date', 'end_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     search_fields = ['question_text']
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'end_date', 'was_published_recently')
+    list_filter = ['pub_date', 'end_date']
 
 
 admin.site.register(Question, QuestionAdmin)
