00c030832f3a67d84f99f40e4b84b9bb7e5cde8d
Kittison <kittison.j@ku.th>
2021-09-12 13:19:03 +0700

    add null=true in model

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date expired')
+    end_date = models.DateTimeField('date expired', null=True)
 
     def __str__(self):
         return self.question_text
